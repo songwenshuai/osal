3419507a7041ef56c2c3f48890436c64fcc805f8
songwenshuai <songwenshuai@sina.com>
2019-10-15 15:53:27 +0800

    【添加】FIFO API。

---
+++ Middlewares/OSAL/Source/Inc/OSAL_Fifo.h
@@ -0,0 +1,188 @@
+/**************************************************************************************************
+  Filename:       OSAL_Fifo.h
+  Revised:        $Date: 2014-06-16 15:12:16 -0700 (Mon, 16 Jun 2014) $
+  Revision:       $Revision: 39036 $
+
+  Description:    This file contains the OSAL Task definition and manipulation functions.
+**************************************************************************************************/
+
+#ifndef OSAL_FIFO_H
+#define OSAL_FIFO_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*!
+ * \brief fifo, data size is byte
+ */
+typedef struct bFIFO{
+	uint32 size;          /*!< fifo size */
+	uint32 length;        /*!< valid data length */
+	uint32 intLine;       /*!< interrupt line */
+	uint8  *pBuffer;      /*!< fifo buffer */
+	uint8  *pWrite;       /*!< pointer to pos for writing */
+	uint8  *pRead;        /*!< pointer to pos for reading */
+} bFIFO_t;
+
+
+/*!
+ * \brief fifo, data size is word(16bit)
+ */
+typedef struct wFIFO{
+	uint32 size;
+	uint32 length;
+	uint32 intLine;
+	uint16 *pBuffer;
+	uint16 *pWrite;
+	uint16 *pRead;
+} wFIFO_t;
+
+/*********************************************************************
+ * MACROS
+ */
+
+/* generic error */
+#define ERR_SUCCESS	 			(0)         /*!< ok */
+#define ERR_UNDEFINE			(-1)        /*!< unknown error */
+#define ERR_PARAMETER			(-2)        /*!< invalid parameter */
+#define ERR_TIME_OUT			(-3)        /*!< time is out */
+#define ERR_PARITY              (-4)        /* */
+
+/* buffer error */
+#define ERR_BUFFER_INITIALIZED  (-10)       /*!< buffer is not initalized */
+#define ERR_BUFFER_EMPTY        (-11)       /*!< buffer is empty */
+#define ERR_BUFFER_FULL         (-12)       /*!< buffer is full */
+
+/* macro definition */
+#define NONE_IRQn	(0xff)                      /*!< invalid interrupt line */
+#define bFIFO_GetLength(a)	((a)->length)       /*!< get data length in fifo */
+#define wFIFO_GetLength(a)	((a)->length)       /*!< get data length in fifo */
+
+/*********************************************************************
+ * FUNCTIONS
+ */
+
+/*!
+ * \brief init the fifo struct. If the interrupt line is valid, enable interrupt
+ * \param [in] pfifo - fifo struct
+ * \param [in] pbuffer - fifo buffer
+ * \param [in] len - buffer size
+ * \param [in] intLine - interrupt line. NONE_IRQn is invalid.
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ */
+int bFIFO_Init(bFIFO_t *pfifo, uint8 *pbuffer, uint32 len, uint8 intLine );
+
+/*!
+ * \brief clear fifo data.
+ * \param [in] pfifo - fifo struct
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ */
+int bFIFO_Clear(bFIFO_t *pfifo);
+
+/*!
+ * \brief put one byte to fifo
+ * \param [in] pfifo - fifo struct
+ * \param [in] data - data which will be writen to fifo
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_FULL fifo is full.
+ */
+int bFIFO_PutByte(bFIFO_t *pfifo, uint8 data);
+
+/*!
+ * \brief get one byte from fifo, fifo is not changed.
+ * \param [in] pfifo - fifo struct
+ * \param [out] data - data
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_EMPTY fifo is empty
+ */
+int bFIFO_Glance(bFIFO_t *pfifo, uint8 *data);
+
+/*!
+ * \brief get one byte from fifo and the data will be delete
+ * \param [in] pfifo - fifo struct
+ * \param [out] data - data
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_EMPTY fifo is empty
+ */
+int bFIFO_GetByte(bFIFO_t *pfifo, uint8 *data);
+
+
+/* fifo for word */
+/*!
+ * \brief init the fifo struct. If the interrupt line is valid, enable interrupt
+ * \param [in] pfifo - fifo struct
+ * \param [in] pbuffer - fifo buffer
+ * \param [in] len - buffer size
+ * \param [in] intSouce - interrupt line. NONE_IRQn is invalid.
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ */
+int wFIFO_Init(wFIFO_t *pfifo, uint16 *pbuffer, uint32 len, uint8 intSouce);
+
+/*!
+ * \brief clear fifo data.
+ * \param [in] pfifo - fifo struct
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ */
+int wFIFO_Clear(wFIFO_t *pfifo);
+
+/*!
+ * \brief put string to fifo
+ * \param [in] pfifo - fifo struct
+ * \param [in] data - data which will be writen to fifo
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_FULL fifo is full.
+ */
+int wFIFO_PutByte(wFIFO_t *pfifo, uint16 data);
+
+/*!
+ * \brief get string from fifo, fifo is not changed.
+ * \param [in] pfifo - fifo struct
+ * \param [out] data - data
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_EMPTY fifo is empty
+ */
+int wFIFO_Glance(wFIFO_t *pfifo, uint16 *data);
+
+/*!
+ * \brief get string from fifo and the data will be delete
+ * \param [in] pfifo - fifo struct
+ * \param [out] data - data
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_EMPTY fifo is empty
+ */
+int wFIFO_GetByte(wFIFO_t *pfifo, uint16 *data);
+
+/*!
+ * \brief put string to fifo
+ * \param [in] pfifo - fifo struct
+ * \param [in] data - data which will be writen to fifo
+ * \retval ERR_SUCCESS ok
+ * \retval ERR_PARAMETER parameter is invalid
+ * \retval ERR_BUFFER_FULL fifo is full.
+ */
+int bFIFO_PutString(bFIFO_t *pfifo, uint8 *data, uint16 len);
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OSAL_FIFO_H */
---
+++ Middlewares/OSAL/Source/Src/OSAL_Fifo.c
@@ -0,0 +1,317 @@
+/**************************************************************************************************
+  Filename:       OSAL_Fifo.c
+  Revised:        $Date: 2014-11-04 15:36:27 -0800 (Tue, 04 Nov 2014) $
+  Revision:       $Revision: 40989 $
+
+  Description:    
+**************************************************************************************************/
+
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "OSAL.h"
+
+#include "OSAL_Fifo.h"
+
+/*********************************************************************
+ * LOCAL VARIABLES
+ */
+static halIntState_t intState;
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+
+/* enable the interrupt */
+static void FIFO_IntEnable(uint8 intLine)
+{
+    if(intLine != NONE_IRQn )
+    { 
+		HAL_EXIT_CRITICAL_SECTION( intState );
+    }   
+}
+
+/* disable the interrupt */
+static void FIFO_IntDisable(uint8 intLine)
+{
+    if(intLine != NONE_IRQn)
+    {
+		HAL_ENTER_CRITICAL_SECTION( intState );
+    }   
+}
+
+/*********************************************************************
+ * FUNCTIONS
+ *********************************************************************/
+
+int bFIFO_Init(bFIFO_t *pfifo, uint8 *pbuffer, uint32 len, uint8 intLine)
+{
+	if((pfifo == NULL) || (pbuffer == NULL) || (len == 0))
+	{
+		return ERR_PARAMETER;
+	}
+	
+    /* initialize the fifo */	
+	pfifo->size = len;		
+	pfifo->pBuffer = pbuffer;
+	pfifo->intLine = intLine;
+	pfifo->length = 0;	/* there is no valid data length */
+	osal_memset(pfifo->pBuffer, 0, pfifo->size);
+	pfifo->pWrite = pfifo->pRead = pfifo->pBuffer; /* write head = read head = buffer head on no valid data */
+	
+	return ERR_SUCCESS;
+}
+
+int bFIFO_Clear(bFIFO_t *pfifo)
+{
+	if(pfifo == NULL)
+	{
+		return ERR_PARAMETER;
+	}
+			
+	FIFO_IntDisable(pfifo->intLine);/* disable the interrupt */
+
+    /* clear the buffer */
+	pfifo->length = 0;	
+	osal_memset(pfifo->pBuffer, 0, pfifo->size);
+	pfifo->pWrite = pfifo->pRead = pfifo->pBuffer;
+
+	FIFO_IntEnable(pfifo->intLine);/* enable the interrupt */
+
+	return ERR_SUCCESS;
+}
+
+int bFIFO_Glance(bFIFO_t *pfifo, uint8 *data)
+{	
+	if((pfifo == NULL) || (data == NULL))
+	{
+		return ERR_PARAMETER;
+	}
+    
+    FIFO_IntDisable(pfifo->intLine); /* disable the interrupt */
+
+    if(pfifo->length == 0) /* fifo is empty */
+	{
+        FIFO_IntEnable(pfifo->intLine);
+		return ERR_BUFFER_EMPTY;
+	}
+	
+	*data = *(pfifo->pRead); /* get the data, but the read head pointer is kept */
+	FIFO_IntEnable(pfifo->intLine); /* enable the interrupt */
+
+	return ERR_SUCCESS;
+}
+
+int bFIFO_GetByte(bFIFO_t *pfifo, uint8 *data)
+{	
+	if((pfifo == NULL) || (data == NULL))
+	{
+		return ERR_PARAMETER;
+	}    
+
+	FIFO_IntDisable(pfifo->intLine); /* disable the interrupt */
+
+	if(pfifo->length == 0) /* fifo is empty */
+	{
+		FIFO_IntEnable(pfifo->intLine);	
+		return ERR_BUFFER_EMPTY;
+	}
+
+	*data = *(pfifo->pRead);/* get the data */
+
+	(pfifo->length)--;
+	(pfifo->pRead)++;
+
+	if(pfifo->pRead >= (pfifo->pBuffer + pfifo->size)) /* roll back */
+	{
+		pfifo->pRead = pfifo->pBuffer;	
+	}
+
+	FIFO_IntEnable(pfifo->intLine);/* enable the interrupt */
+
+	return ERR_SUCCESS;
+}
+
+int bFIFO_PutByte(bFIFO_t *pfifo, uint8 data)
+{
+	if(pfifo == NULL)
+	{
+		return ERR_PARAMETER;
+	}
+
+	FIFO_IntDisable(pfifo->intLine); /* disable the interrupt */
+
+	if(pfifo->length == pfifo->size) /* fifo is empty */
+	{
+		FIFO_IntEnable(pfifo->intLine);
+		return ERR_BUFFER_FULL;
+	}
+	
+	*(pfifo->pWrite) = data;
+
+	(pfifo->length)++;
+	(pfifo->pWrite)++;
+
+	if(pfifo->pWrite >= (pfifo->pBuffer + pfifo->size)) /* buffer roll back */
+	{
+		pfifo->pWrite = pfifo->pBuffer;	
+	}
+
+	FIFO_IntEnable(pfifo->intLine); /* enable the interrupt */
+
+	return ERR_SUCCESS;
+}
+
+int bFIFO_PutString(bFIFO_t *pfifo, uint8 *data, uint16 len)
+{
+	if(pfifo == NULL)
+	{
+		return ERR_PARAMETER;
+	}
+
+	FIFO_IntDisable(pfifo->intLine); /* disable the interrupt */
+
+	if(pfifo->length+len > pfifo->size) /* fifo has no enough space */
+	{
+		FIFO_IntEnable(pfifo->intLine);
+		return ERR_BUFFER_FULL;
+	}
+
+	while(len--)
+	{
+		*(pfifo->pWrite) = *data++;
+
+		(pfifo->length)++;
+		(pfifo->pWrite)++;
+
+		if(pfifo->pWrite >= (pfifo->pBuffer + pfifo->size)) /* buffer roll back */
+		{
+			pfifo->pWrite = pfifo->pBuffer;	
+		}
+	}
+	
+
+	FIFO_IntEnable(pfifo->intLine); /* enable the interrupt */
+
+	return ERR_SUCCESS;
+}
+
+int wFIFO_Init(wFIFO_t *pfifo, uint16 *pbuffer, uint32 len, uint8 intLine)
+{
+	if((pfifo == NULL) || (pbuffer == NULL) || (len == 0))
+	{
+		return ERR_PARAMETER;
+	}
+
+	pfifo->size = len;		
+	pfifo->pBuffer = pbuffer;
+	pfifo->intLine = intLine;
+	pfifo->length = 0;	
+	osal_memset(pfifo->pBuffer, 0, pfifo->size);
+	pfifo->pWrite = pfifo->pRead = pfifo->pBuffer;
+	
+	return ERR_SUCCESS;
+}
+
+int wFIFO_Clear(wFIFO_t *pfifo)
+{
+	if(pfifo == NULL)
+	{
+		return ERR_PARAMETER;
+	}
+			
+	FIFO_IntDisable(pfifo->intLine);
+
+	pfifo->length = 0;	
+	osal_memset(pfifo->pBuffer, 0, pfifo->size);
+	pfifo->pWrite = pfifo->pRead = pfifo->pBuffer;
+
+	FIFO_IntEnable(pfifo->intLine);
+
+	return ERR_SUCCESS;
+}
+
+int wFIFO_Glance(wFIFO_t *pfifo, uint16 *data)
+{	
+	if((pfifo == NULL) || (data == NULL))
+	{
+		return ERR_PARAMETER;
+	}
+
+	FIFO_IntDisable(pfifo->intLine);
+
+	if(pfifo->length == 0)
+	{
+		FIFO_IntEnable(pfifo->intLine);	
+		return ERR_BUFFER_EMPTY;
+	}
+
+	*data = *(pfifo->pRead);
+
+	FIFO_IntEnable(pfifo->intLine);
+
+	return ERR_SUCCESS;
+}
+
+int wFIFO_GetByte(wFIFO_t *pfifo, uint16 *data)
+{	
+	if((pfifo == NULL) || (data == NULL))
+	{
+		return ERR_PARAMETER;
+	}
+
+	FIFO_IntDisable(pfifo->intLine);
+
+	if(pfifo->length == 0)
+	{
+		FIFO_IntEnable(pfifo->intLine);	
+		return ERR_BUFFER_EMPTY;
+	}
+
+	*data = *(pfifo->pRead);
+
+	(pfifo->length)--;
+	(pfifo->pRead)++;
+
+	if(pfifo->pRead >= (pfifo->pBuffer + pfifo->size))
+	{
+		pfifo->pRead = pfifo->pBuffer;	
+	}
+
+	FIFO_IntEnable(pfifo->intLine);
+
+	return ERR_SUCCESS;
+}
+
+int wFIFO_PutByte(wFIFO_t *pfifo, uint16 data)
+{
+	if(pfifo == NULL)
+	{
+		return ERR_PARAMETER;
+	}
+
+	FIFO_IntDisable(pfifo->intLine);
+
+	if(pfifo->length == pfifo->size)
+	{
+		FIFO_IntEnable(pfifo->intLine);
+		return ERR_BUFFER_FULL;
+	}
+	
+	*(pfifo->pWrite) = data;
+
+	(pfifo->length)++;
+	(pfifo->pWrite)++;
+
+	if(pfifo->pWrite >= (pfifo->pBuffer + pfifo->size))
+	{
+		pfifo->pWrite = pfifo->pBuffer;	
+	}
+
+	FIFO_IntEnable(pfifo->intLine);
+
+	return ERR_SUCCESS;
+}
+
+
